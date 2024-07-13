3d487d3d461790171c828f5862d1a031c618f96d
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-13 19:15:00 -0300

    feat: update origin url front-end

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ import { getParticipant } from "./routes/get-participant";
 const app = fastify();
 
 app.register(cors, {
-  origin: "https://nlw-journey-trilha-react.vercel.app",
+  origin: env.WEB_BASE_URL || "https://nlw-journey-trilha-react.vercel.app",
 });
 
 app.setValidatorCompiler(validatorCompiler);
