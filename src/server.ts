affb617116443d9e32573df8a4a500c8d9b79374
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-13 16:44:11 -0300

    feat: add confirm trip route

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,24 @@
-console.log("Hello World!!!");
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { CreateTrip } from "./routes/create-trip";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { ConfirmTrip } from "./routes/confirm-trip";
+
+const app = fastify();
+
+app.register(cors, {
+  origin: "*",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(CreateTrip);
+app.register(ConfirmTrip);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server running!");
+});
